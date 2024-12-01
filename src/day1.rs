c172754cf357e6739c471271918f1050302593d1
Jan De Kock <jan@twofold.be>
2024-12-01 17:38:40 +0100

    It's been a while...

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,34 @@
-pub(crate) fn day1(input: &str) -> i32 {
-    0
+use std::collections::{HashMap, HashSet};
+use std::iter::zip;
+
+pub(crate) fn part1(input: &str) -> i32 {
+    let (mut array1, mut array2) = parse(input);
+
+    array1.sort();
+    array2.sort();
+
+    zip(array1, array2).map(|(a, b)| (b - a).abs()).sum()
+}
+
+pub(crate) fn part2(input: &str) -> i32 {
+    let (array1, array2) = parse(input);
+
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    array2.iter().for_each(|&x| {
+        map.entry(x).and_modify(|x| *x += 1).or_insert(1);
+    });
+
+    array1.iter().map(|&x| x * *map.entry(x).or_default()).sum()
+}
+
+fn parse(input: &str) -> (Vec<i32>, Vec<i32>) {
+    let numbers = input
+        .lines()
+        .flat_map(|line| line.split_whitespace().map(|s| s.parse().unwrap()))
+        .collect::<Vec<i32>>();
+
+    (
+        numbers.chunks_exact(2).map(|i| i[0]).collect::<Vec<_>>(),
+        numbers.chunks_exact(2).map(|i| i[1]).collect::<Vec<_>>(),
+    )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use crate::day1::day1;
+use std::fs::read_to_string;
 
 mod day1;
 
 fn main() {
-    println!("{}", day1(""))
+    println!("{}", day1::part1(&read_to_string("day1.txt").unwrap()));
+    println!("{}", day1::part2(&read_to_string("day1.txt").unwrap()));
 }
